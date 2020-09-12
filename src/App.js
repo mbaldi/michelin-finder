eac0204bd21f6642edfa49301098bbfcf4fecb4d
Luis Baldiviezo <luis@codefabric.io>
2020-09-12 18:52:34 -0400

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+export const client = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const App = () => {
+  const [message, setMessage] = useState('Loading...');
+
+  useEffect(() => {
+    const getHello = async () => {
+      const hello = await client.get('/hello');
+      setMessage(hello.data.message);
+    };
+    getHello();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{message}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
